1c531cf69d771721849fb388baf02326ef731fa0
Humberto <bethox@gmail.com>
2015-10-26 16:04:07 +0100

    Fixed the levenshtein_distance function.

--- drugBankDistance.py
+++ drugBankDistance.py
@@ -10,7 +10,7 @@ def levenshtein_dist(drugs):
     for i in range(0, len(drug_list)):
         for j in range(i+1, len(drug_list)):
             # There are names that are not string or unicode
-            if (isinstance(drugs[drug_list[i]].name, str) and isinstance(drugs[drug_list[j]].name.isalpha(), str)):
+            if (isinstance(drugs[drug_list[i]].name, str) and isinstance(drugs[drug_list[j]].name, str)):
                 d = distance(drugs[drug_list[i]].name, drugs[drug_list[j]].name)
                 dist.append(((drugs[drug_list[i]].name, drugs[drug_list[j]].name), d))
 
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ __author__ = 'root'
 from drugBankAcessor_ET import  mapDrugBankFromFile
 from sampleDrugBank import sampleFirstNdrugsFromXml, sampleXlinesfromXml
 from drugBankDistance import levenshtein_dist
-from Levenshtein import distance
 
 if __name__ == '__main__':
 
@@ -17,11 +16,12 @@ if __name__ == '__main__':
 
     #filename = '/home/iva/DMKM/DrugBank/drugbank.xml'
     #filename = '/root/PycharmProjects/drugbank/sampleN.xml'
-    filename = '/Volumes/Local/Users/humberto/projects/drugbank/drugbank.xml'
+    filename = '/Volumes/Local/Users/humberto/projects/drugbank/drugbank_short.xml'
     drugs = mapDrugBankFromFile(filename)
     print len(drugs)
     #for key, value in drugs.iteritems():
     #     value.printout()
     d = levenshtein_dist(drugs)
+    print d
 
 
